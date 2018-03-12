86c028cd504043749aadcee211cd3c7a12fa939b
Billy <billy_white.88@yahoo.com>
2018-03-12 17:01:56 -0500

    contact manager

---
+++ src/contact.java
@@ -0,0 +1,29 @@
+
+
+public class contact {
+    private String contactName;
+    private String phoneNumber;
+
+    public contact(String contactName, String phoneNumber) {
+        this.contactName = contactName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+}
